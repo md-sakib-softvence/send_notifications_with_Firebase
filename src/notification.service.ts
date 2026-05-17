6dead2b56077f75a2cf9ae49d0087b0e8c0461ba
md-sakib-softvence <sakibsoftvence@gmail.com>
2026-05-17 14:54:48 +0600

    Update notification.service.ts
    chage the notifications file

--- src/notification.service.ts
+++ src/notification.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable, Logger, InternalServerErrorException } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { FirebaseService } from './firebase.service';
@@ -43,109 +44,109 @@ export class NotificationService {
     }
 
 
-    @Cron(CronExpression.EVERY_MINUTE)
-    async checkAndSendDailyNotification() {
-        this.cronHitCount++; // Increment the counter every time it runs!
-        this.logger.log(`Running daily notification check... (Hit count: ${this.cronHitCount})`);
-        try {
-            const firestore = this.firebaseService.getFirestore();
-            const remindersRef = firestore.collection('reminder');
-            const snapshot = await remindersRef.get();
+    // @Cron(CronExpression.EVERY_MINUTE)
+    // async checkAndSendDailyNotification() {
+    //     this.cronHitCount++; // Increment the counter every time it runs!
+    //     this.logger.log(`Running daily notification check... (Hit count: ${this.cronHitCount})`);
+    //     try {
+    //         const firestore = this.firebaseService.getFirestore();
+    //         const remindersRef = firestore.collection('reminder');
+    //         const snapshot = await remindersRef.get();
 
-            if (snapshot.empty) {
-                return;
-            }
+    //         if (snapshot.empty) {
+    //             return;
+    //         }
 
-            const now = new Date();
+    //         const now = new Date();
 
-            for (const doc of snapshot.docs) {
-                const data = doc.data();
-                const token = data.token;
-                const timeData = data.time;
-                const endDateData = data.endDate; // Get the endDate if provided
+    //         for (const doc of snapshot.docs) {
+    //             const data = doc.data();
+    //             const token = data.token;
+    //             const timeData = data.time;
+    //             const endDateData = data.endDate; // Get the endDate if provided
 
-                if (!token || !timeData) {
-                    continue;
-                }
+    //             if (!token || !timeData) {
+    //                 continue;
+    //             }
 
-                // Check if the reminder has reached its 14-day limit
-                if (endDateData) {
-                    const expirationTime = (typeof endDateData.toDate === 'function') ? endDateData.toDate() : new Date(endDateData);
+    //             // Check if the reminder has reached its 14-day limit
+    //             if (endDateData) {
+    //                 const expirationTime = (typeof endDateData.toDate === 'function') ? endDateData.toDate() : new Date(endDateData);
 
-                    if (now >= expirationTime) {
-                        this.logger.log(`Reminder doc ${doc.id} has reached its endDate. Deleting from database.`);
-                        await doc.ref.delete();
-                        continue; // Skip the rest of the loop so we don't send a notification
-                    }
-                }
-
-
-                let reminderTime: Date;
-                if (typeof timeData.toDate === 'function') {
-                    reminderTime = timeData.toDate();
-                } else if (typeof timeData === 'number') {
-
-                    reminderTime = new Date(timeData);
-                } else {
-
-                    reminderTime = new Date(timeData);
-                }
+    //                 if (now >= expirationTime) {
+    //                     this.logger.log(`Reminder doc ${doc.id} has reached its endDate. Deleting from database.`);
+    //                     await doc.ref.delete();
+    //                     continue; // Skip the rest of the loop so we don't send a notification
+    //                 }
+    //             }
 
 
-                if (now >= reminderTime) {
-                    this.logger.log(`Reminder expired for doc ${doc.id}. Sending notification...`);
+    //             let reminderTime: Date;
+    //             if (typeof timeData.toDate === 'function') {
+    //                 reminderTime = timeData.toDate();
+    //             } else if (typeof timeData === 'number') {
 
-                    const appTitle = data.appTitle;
-                    const appId = data.appId;
+    //                 reminderTime = new Date(timeData);
+    //             } else {
 
-                    const titleStr = appTitle ? `Time to test ${appTitle}!` : (data.title || 'Daily Reminder');
-                    const bodyStr = data.body || 'Keep your streak alive by testing today.';
-
-                    const payload: SendNotificationDto = {
-                        token: token,
-                        title: titleStr,
-                        body: bodyStr,
-                        data: {
-                            type: 'reminder',
-                            ...(appId && { appId }),
-                        }
-                    };
-
-                    try {
-                        await this.sendPushNotification(payload);
+    //                 reminderTime = new Date(timeData);
+    //             }
 
 
-                        await firestore.collection('notifications').add({
-                            title: payload.title,
-                            message: payload.body,
-                            type: 'reminder',
-                            isRead: false,
-                            createdAt: new Date(),
-                            userId: data.userId || null,
-                            data: {
-                                type: 'reminder',
-                                ...(appId && { appId }),
-                            }
-                        });
+    //             if (now >= reminderTime) {
+    //                 this.logger.log(`Reminder expired for doc ${doc.id}. Sending notification...`);
 
-                    } catch (error) {
-                        this.logger.error(`Failed to send push for reminder ${doc.id}. It may have an invalid token.`, error);
-                    }
+    //                 const appTitle = data.appTitle;
+    //                 const appId = data.appId;
 
-                    const nextDay = new Date(reminderTime);
-                    nextDay.setMinutes(nextDay.getMinutes() + 1);
+    //                 const titleStr = appTitle ? `Time to test ${appTitle}!` : (data.title || 'Daily Reminder');
+    //                 const bodyStr = data.body || 'Keep your streak alive by testing today.';
 
-                    await doc.ref.update({
-                        time: nextDay.toISOString()
-                    });
+    //                 const payload: SendNotificationDto = {
+    //                     token: token,
+    //                     title: titleStr,
+    //                     body: bodyStr,
+    //                     data: {
+    //                         type: 'reminder',
+    //                         ...(appId && { appId }),
+    //                     }
+    //                 };
 
-                    this.logger.log(`Reminder ${doc.id} updated to next minute: ${nextDay.toISOString()}`);
-                }
-            }
-        } catch (error) {
-            this.logger.error('Error in cron job while checking/sending notification:', error);
-        }
-    }
+    //                 try {
+    //                     await this.sendPushNotification(payload);
+
+
+    //                     await firestore.collection('notifications').add({
+    //                         title: payload.title,
+    //                         message: payload.body,
+    //                         type: 'reminder',
+    //                         isRead: false,
+    //                         createdAt: new Date(),
+    //                         userId: data.userId || null,
+    //                         data: {
+    //                             type: 'reminder',
+    //                             ...(appId && { appId }),
+    //                         }
+    //                     });
+
+    //                 } catch (error) {
+    //                     this.logger.error(`Failed to send push for reminder ${doc.id}. It may have an invalid token.`, error);
+    //                 }
+
+    //                 const nextDay = new Date(reminderTime);
+    //                 nextDay.setMinutes(nextDay.getMinutes() + 1);
+
+    //                 await doc.ref.update({
+    //                     time: nextDay.toISOString()
+    //                 });
+
+    //                 this.logger.log(`Reminder ${doc.id} updated to next minute: ${nextDay.toISOString()}`);
+    //             }
+    //         }
+    //     } catch (error) {
+    //         this.logger.error('Error in cron job while checking/sending notification:', error);
+    //     }
+    // }
 
     async checkAndSendDailyNotification1() {
         this.logger.log('Running daily notification check...');
@@ -209,6 +210,7 @@ export class NotificationService {
                         body: bodyStr,
                         data: {
                             type: 'reminder',
+                            skip_firestore_save: 'true',
                             ...(appId && { appId }),
                         }
                     };
@@ -232,7 +234,8 @@ export class NotificationService {
                         this.logger.error(`Failed to send push for reminder ${doc.id}. It may have an invalid token.`, error);
                     }
 
-                    const nextDay = new Date(reminderTime);
+                    // Use 'now' instead of 'reminderTime' to prevent rapid retries catching up
+                    const nextDay = new Date(); 
                     nextDay.setMinutes(nextDay.getMinutes() + 1);
 
                     await doc.ref.update({
